71ea9c8265d61e51744520a82865182987ba9842
Megan Farley <megfarley4@gmail.com>
2018-10-15 18:18:13 -0500

    finished lab

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+
+    attr_accessor :word
+
+    def initialize(word)
+        self.word = word
+    end
+
+    def match(words_array)
+        new_array = []
+        words_array.each do |words|
+            #binding.pry
+            if words.split("").sort == self.word.split("").sort
+                new_array << words
+            end
+        end
+        new_array
+    end    
+
+end
